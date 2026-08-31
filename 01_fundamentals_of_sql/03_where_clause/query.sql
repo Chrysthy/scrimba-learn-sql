2e0eeae5c0172ce144385840d32386cd462c2a8e
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-31 19:57:11 -0300

    feat: add challenge

--- 01_fundamentals_of_sql/03_where_clause/query.sql
+++ 01_fundamentals_of_sql/03_where_clause/query.sql
@@ -3,5 +3,15 @@
 		where the color is black
 */
 
-SELECT brand, model, color, price FROM cars
-    WHERE color = 'black';
+-- SELECT brand, model, color, price FROM cars
+--     WHERE color = 'black';
+
+
+
+/*
+	Select the brand, model, condition and price from cars
+		where the condition equals 0
+*/
+
+SELECT brand, model, condition, price FROM cars
+	WHERE condition = 0
