9d744bf729ac9dc5d98001d23bb51aafe73ee616
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-31 19:50:20 -0300

    docs: comment previous query and add execution note

--- 01_fundamentals_of_sql/02_selecting_columns/query.sql
+++ 01_fundamentals_of_sql/02_selecting_columns/query.sql
@@ -1,6 +1,7 @@
 -- Show me the brand, model and price of our cars
 
-SELECT brand, model, price FROM cars;
+-- SELECT brand, model, price FROM cars;
+-- Remember, you can't run two queries in the same file with this setup.
 
 
 
