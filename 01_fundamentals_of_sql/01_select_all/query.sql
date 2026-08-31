642bf89d92f1c4d11ff9782d59bba725884fd565
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-30 21:42:38 -0300

    feat: add SQL query to list retro cars.

---
+++ 01_fundamentals_of_sql/01_select_all/query.sql
@@ -0,0 +1,7 @@
+-- Retro Rides
+-- Let's take stock of all the retro rides in our database. 
+--  To run this query, open the terminal and run the following commands:
+-- cd .\01_fundamentals_of_sql\01_select_all
+-- node ..\..\index.js
+
+SELECT * FROM cars;
