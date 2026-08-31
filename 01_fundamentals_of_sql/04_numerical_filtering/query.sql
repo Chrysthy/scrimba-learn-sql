aaec37b10b41e8a02a4a8ccc9b3e808c2682c576
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-31 20:59:42 -0300

    feat: add numerical filtering example

---
+++ 01_fundamentals_of_sql/04_numerical_filtering/query.sql
@@ -0,0 +1,12 @@
+/*
+	Select the brand, model, condition and price from cars
+		find results where the condition is greater than 3
+*/
+
+SELECT brand, model, condition, price 
+FROM cars
+WHERE condition > 3;
+
+
+
+
