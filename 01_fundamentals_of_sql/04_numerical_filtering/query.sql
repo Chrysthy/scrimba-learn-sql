2b28002184f5ea13b8ff7216d29295733c13e366
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-31 21:04:58 -0300

    feat: ass challenge

--- 01_fundamentals_of_sql/04_numerical_filtering/query.sql
+++ 01_fundamentals_of_sql/04_numerical_filtering/query.sql
@@ -3,10 +3,15 @@
 		find results where the condition is greater than 3
 */
 
+-- SELECT brand, model, condition, price 
+-- FROM cars
+-- WHERE condition > 3;
+
+
+/*
+Select the brand, model, condition and price from cars find results where the price is less than $50,000
+*/
 SELECT brand, model, condition, price 
 FROM cars
-WHERE condition > 3;
-
-
-
+WHERE price < 50000;
 
