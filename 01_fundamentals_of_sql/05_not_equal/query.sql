1416bf0bea9aead94bf767bca6717558dd6749f1
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-01 21:49:23 -0300

    feat: add not equal example 2

--- 01_fundamentals_of_sql/05_not_equal/query.sql
+++ 01_fundamentals_of_sql/05_not_equal/query.sql
@@ -5,6 +5,16 @@
 
 -- 1965 was a bad year for me... Please don't show me cars from that year.
 
-SELECT brand, model, year, price FROM cars
-    WHERE year != 1965;
+ SELECT brand, model, year, price FROM cars
+     WHERE year != 1965;
     -- ou WHERE year <> 1965; <> pode ser usado como operador de desigualdade também
+
+
+
+/*
+	Find cars which are not yellow
+		Select the brand, model, price and color
+*/
+
+SELECT brand, model, price, color FROM cars
+    WHERE color <> 'yellow';
