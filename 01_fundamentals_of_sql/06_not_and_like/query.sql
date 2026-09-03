3105d5f9141f00aff115af267873862365b6be5a
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-02 21:05:17 -0300

    feat: add challenge

--- 01_fundamentals_of_sql/06_not_and_like/query.sql
+++ 01_fundamentals_of_sql/06_not_and_like/query.sql
@@ -31,3 +31,13 @@ SELECT brand, model, color, year FROM cars
 -- Podemos adicionar o not para negar a condição, ou seja, encontrar qualquer carro que não tenha a cor verde.
 SELECT brand, model, color, year FROM cars
     WHERE color NOT LIKE '%green%';
+
+
+
+/*
+	Select the brand, model, color and year for cars
+		where the model is 'DB' followed by any other single character
+*/
+
+SELECT brand, model, color, year FROM cars
+    WHERE model LIKE 'DB_';
