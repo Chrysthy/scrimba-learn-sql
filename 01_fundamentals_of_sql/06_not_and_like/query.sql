de780d6b60b0422671a427b9737339f9da36765a
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-02 21:02:48 -0300

    feat: add NOT and LIKE explanation

---
+++ 01_fundamentals_of_sql/06_not_and_like/query.sql
@@ -0,0 +1,33 @@
+ /*
+
+% - any number of any characters
+_ - one of any character, exactly one character
+
+
+'%green%' matches any string that contains the substring "green" anywhere in it.
+- 'light green'
+- 'dark green'
+- 'greenish-yellow'
+
+
+'_-Type' matches any string that has exactly one character before the substring "Type".
+
+- 'X-Type'
+- 'S-Type'
+- 'E-Type'
+
+*/
+
+
+/*
+	Select the brand, model, color and year
+		find any car where the color includes 'green'
+*/
+
+SELECT brand, model, color, year FROM cars
+    WHERE color LIKE '%green%';
+
+
+-- Podemos adicionar o not para negar a condição, ou seja, encontrar qualquer carro que não tenha a cor verde.
+SELECT brand, model, color, year FROM cars
+    WHERE color NOT LIKE '%green%';
