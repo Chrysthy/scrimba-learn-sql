6e75dc3e0ffa32c9676466392872a657745e25de
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-04 15:12:33 -0300

    feat: add challenge

--- 01_fundamentals_of_sql/08_between/query.sql
+++ 01_fundamentals_of_sql/08_between/query.sql
@@ -12,3 +12,21 @@ SELECT brand, model, year, price FROM cars
 -- Using between - BETWEEN já inclui os dois extremos
 SELECT brand, model, year, price FROM cars
     WHERE year BETWEEN 1980 AND 1989;
+
+
+
+
+
+/*
+	Select brand, model, condition, color and price from cars
+		where the price is between $20,000 and $60,000
+		and the condition is between 1 and 3
+		and the color contains red
+*/
+
+SELECT brand, model, condition, color, price FROM cars  
+    WHERE price BETWEEN 20000 AND 60000
+    AND condition BETWEEN 1 AND 3
+    AND color LIKE '%red%'; -- vai encontrar qualquer valor que contenha red
+
+    -- AND color = 'red'; vai encontrar somente registros em que a coluna seja exatamente red
