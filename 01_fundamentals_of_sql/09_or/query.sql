9bde7219ac3ebac6cfa00b74e4c7270a2c2f2012
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-07 09:56:11 -0300

    feat: add new challenge

--- 01_fundamentals_of_sql/09_or/query.sql
+++ 01_fundamentals_of_sql/09_or/query.sql
@@ -9,3 +9,15 @@ SELECT brand, model, condition, price FROM cars
 	WHERE (price < 250000
 	OR brand = 'Porsche')
 	AND condition > 3;
+
+
+
+/*
+	Search for brand, model, color, year and price of cars
+		where the color is a shade of red
+		or the year is between 1960 and 1969
+*/
+
+SELECT brand, model, color, year, price FROM cars
+WHERE color LIKE '%red%'
+OR year BETWEEN 1960 AND 1969;
