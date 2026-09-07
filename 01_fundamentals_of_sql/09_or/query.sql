745b1043ad74d16215aeaf5940a0ada5912f068a
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-07 09:46:51 -0300

    feat: add one more condition to use ( )

--- 01_fundamentals_of_sql/09_or/query.sql
+++ 01_fundamentals_of_sql/09_or/query.sql
@@ -2,8 +2,10 @@
 	Find the brand, model, condition and price of cars
 		where the price is less than $250,000
 		or the brand is Porsche
+        only show cars with condition > 3
 */
 
 SELECT brand, model, condition, price FROM cars
-    WHERE price < 250000 
-    OR brand = 'Porche';
+	WHERE (price < 250000
+	OR brand = 'Porsche')
+	AND condition > 3;
