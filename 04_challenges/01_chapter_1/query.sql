7745ee0906a859f1fbdbd8292546e38c8799f1c4
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-09 15:41:30 -0300

    feat: add Challenge 01

---
+++ 04_challenges/01_chapter_1/query.sql
@@ -0,0 +1,13 @@
+-- Challenge 01
+
+/*
+	Select brand, model, and color from cars
+		where the color is 'red'
+		and the brand is not 'Ferrari'
+		and the car has not been sold
+*/
+
+SELECT brand, model, color FROM cars
+	WHERE color = 'red'
+    AND brand != 'Ferrari'
+    AND sold IS FALSE;
