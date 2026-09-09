c56269defdf9db905b253a2633f85b8ff58bddf5
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-08 21:48:48 -0300

    feat: add challenge 2

--- 01_fundamentals_of_sql/10_in/query.sql
+++ 01_fundamentals_of_sql/10_in/query.sql
@@ -21,3 +21,19 @@ SELECT brand, model, condition, year FROM cars
     WHERE year IN (1961, 1963, 1965, 1967, 1969)
     AND condition >= 3
     AND sold IS FALSE;
+
+
+
+
+/*
+	Select brand, model, price and sold from cars
+		filter out any cars which are sold
+		show cars where the brand is none of ('Ford', 'Triumph', 'Chevrolet', 'Dodge')
+		or the price is less than $50000
+*/
+
+SELECT brand, model, price, sold FROM cars
+    WHERE (
+        brand NOT IN ('Ford', 'Triumph', 'Chevrolet', 'Dodge')
+        OR price > 50000
+    )   AND sold IS FALSE;
