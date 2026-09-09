a82a0c1ad4327782c796002d515f73b720e628b2
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-08 21:42:57 -0300

    feat: add challenge 1

--- 01_fundamentals_of_sql/10_in/query.sql
+++ 01_fundamentals_of_sql/10_in/query.sql
@@ -7,3 +7,17 @@
 SELECT brand, model, price, sold FROM cars
     WHERE brand IN ('Ford', 'Chevrolet', 'Ferrari')
     AND sold IS FALSE;
+
+
+
+/*
+	Select the brand, model, condition and year from cars
+		Where the year is 1961, 1963, 1965, 1967 or 1969
+		and the condition is 3 or higher
+		and sold is false
+*/
+
+SELECT brand, model, condition, year FROM cars
+    WHERE year IN (1961, 1963, 1965, 1967, 1969)
+    AND condition >= 3
+    AND sold IS FALSE;
