293162f2c38712479c6ee3aff395575edbadb1f5
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-10 14:03:38 -0300

    feat: add challenge 2

--- 04_challenges/01_chapter_1/query.sql
+++ 04_challenges/01_chapter_1/query.sql
@@ -11,3 +11,19 @@ SELECT brand, model, color FROM cars
 	WHERE color = 'red'
     AND brand != 'Ferrari'
     AND sold IS FALSE;
+
+
+
+-- Challenge 02
+
+/*
+	Select brand, model, and color from cars
+		where the color is not red, blue, or white
+		and the brand is none of: Aston Martin, Bentley or Jaguar
+		and sold is false
+*/
+
+SELECT brand, model, color FROM cars
+	WHERE color NOT IN ('red', 'blue', 'white')
+	AND brand NOT IN ('Aston Martin', 'Bentley', 'Jaguar')
+	AND sold IS FALSE;
