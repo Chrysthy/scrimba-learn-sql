14ca8dcc0caccc0216e5bc1b71ced58b34525e01
Chrystine Martins <chrystine.martins@outlook.com>
2026-09-19 13:05:05 -0300

    feat: add challenge

--- 01_fundamentals_of_sql/12_limit/query.sql
+++ 01_fundamentals_of_sql/12_limit/query.sql
@@ -7,3 +7,20 @@
 SELECT brand, model, year, price FROM cars
     ORDER BY price DESC 
     LIMIT 1;
+
+
+
+
+/*
+	Select the brand, model, color and price from cars
+		where the color is a shade of 'red'
+		and sold is false
+		order by price
+		limit the results to 5
+*/
+
+SELECT brand, model, color, price FROM cars
+	WHERE color LIKE '%red%' AND
+	sold IS FALSE 
+	ORDER BY price
+	LIMIT 5
