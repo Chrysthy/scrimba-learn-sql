553dd669bb6ae08cf6e4d93b60b3cdd5ad1d73ed
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-22 15:41:40 -0300

    feat: add AVG example

--- 01_fundamentals_of_sql/14_max_min_avg/query.sql
+++ 01_fundamentals_of_sql/14_max_min_avg/query.sql
@@ -1,6 +1,5 @@
 -- MAX(): retorna o maior valor de uma coluna.
 
-
 /*
 	Select the maximum retail price
 		where sold is true
@@ -10,3 +9,20 @@
 
 SELECT MAX(price) AS most_expensive FROM cars
     WHERE sold IS TRUE;
+
+
+
+
+-- AVG(): calcula a média dos valores de uma coluna.
+
+/*
+	Use the AVG aggregate function to find the average price
+		where the brand is Bentley
+*/
+
+SELECT FLOOR(AVG(price)) as average FROM cars
+    WHERE brand = 'Bentley';
+
+
+-- FLOOR() Arredonda para baixo, para o menor número inteiro.
+-- CEILING() ou CEIL() Arredonda para cima, para o próximo número inteiro.
