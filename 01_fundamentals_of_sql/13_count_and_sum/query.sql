e76e966cae79b239ac20ef07a806cf3f801117d4
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-22 15:21:40 -0300

    feat: add sum example

--- 01_fundamentals_of_sql/13_count_and_sum/query.sql
+++ 01_fundamentals_of_sql/13_count_and_sum/query.sql
@@ -23,8 +23,13 @@ SELECT COUNT(*) FROM cars
 
 
 
+
 /*
 	Sum the price of cars
 		where sold is true
 	Use the alias total_earnings in your output
 */
+
+
+SELECT SUM(price) AS total_earnings FROM cars
+	WHERE sold IS TRUE;
