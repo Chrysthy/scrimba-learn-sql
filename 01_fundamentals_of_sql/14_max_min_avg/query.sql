d9ff258359e8f452393813f5e80d5797caefc7eb
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-22 15:25:51 -0300

    feat: add max example

---
+++ 01_fundamentals_of_sql/14_max_min_avg/query.sql
@@ -0,0 +1,12 @@
+-- MAX(): retorna o maior valor de uma coluna.
+
+
+/*
+	Select the maximum retail price
+		where sold is true
+	Use most_expensive as an alias
+*/
+
+
+SELECT MAX(price) AS most_expensive FROM cars
+    WHERE sold IS TRUE;
