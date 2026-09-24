c764d1d22c1c9f1418848502a8bcb9904903a5fc
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-24 15:28:26 -0300

    feat: add GROUP BY explanation

---
+++ 01_fundamentals_of_sql/15_group_by/query.sql
@@ -0,0 +1,20 @@
+-- GROUP BY é usado para agrupar linhas que têm o mesmo valor em uma coluna.
+-- Ele é muito usado junto com funções de agregação, como:
+
+-- COUNT()
+-- SUM()
+-- AVG()
+-- MIN()
+-- MAX()
+
+/*
+	Select the brand, and a count of the brand from cars
+		alias the count as brand_count
+		group by the brand column
+*/
+
+
+SELECT brand, COUNT(brand) AS brand_count FROM cars
+    GROUP BY brand;
+
+-- o SQL vai juntar todos os carros da mesma marca em um grupo e depois contar quantos existem em cada grupo.
