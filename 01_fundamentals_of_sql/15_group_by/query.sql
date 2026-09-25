2434364b3692928945322b15d9697b0c4da0aa48
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-25 15:35:56 -0300

    feat: add new example

--- 01_fundamentals_of_sql/15_group_by/query.sql
+++ 01_fundamentals_of_sql/15_group_by/query.sql
@@ -28,3 +28,25 @@ SELECT brand, COUNT(brand) AS brand_count FROM cars
 
 SELECT condition, COUNT(condition) FROM cars
 	GROUP BY(condition);
+
+
+
+
+/*
+	Select:
+		* the brand
+		* a count of the brand
+		* and an average of the price for each brand
+		* round the average down to the nearest number
+		* alias the average as 'AVG' in your output
+	From cars where
+		the car has not been sold
+	Group the table by brand.
+*/
+
+
+
+SELECT brand, COUNT(brand), FLOOR(AVG(price)) AS AVG 
+	FROM cars
+	WHERE sold IS FALSE
+	GROUP BY brand; 
