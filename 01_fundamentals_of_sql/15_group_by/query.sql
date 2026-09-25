4957cb82d7fbb29cd3310cadccff7aaf2da8f150
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-25 15:17:41 -0300

    feat: add group by example

--- 01_fundamentals_of_sql/15_group_by/query.sql
+++ 01_fundamentals_of_sql/15_group_by/query.sql
@@ -18,3 +18,13 @@ SELECT brand, COUNT(brand) AS brand_count FROM cars
     GROUP BY brand;
 
 -- o SQL vai juntar todos os carros da mesma marca em um grupo e depois contar quantos existem em cada grupo.
+
+
+
+/*
+	Select the condition, and a count of the condition from cars
+		group by the condition column
+*/
+
+SELECT condition, COUNT(condition) FROM cars
+	GROUP BY(condition);
